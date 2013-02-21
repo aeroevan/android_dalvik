09bf7689f93e0e4e17cfc76e786ec0dbb4eb2349
Henrik Smiding <henrik.smiding@stericsson.com>
2013-02-21 23:11:53 +0100

    JIT: Hoist loop load operations above loop.
    Hoists Dalvik frame load operations outside of loops, when possible.
    This improves performance in most loops, and is noticable in many
    applications.
    
    Change-Id: Ibe42b509b50a13f19758fd923f31d703e33a51c6
    Signed-off-by: Henrik Smiding <henrik.smiding@stericsson.com>
    Signed-off-by: Patrik Ryd <patrik.ryd@stericsson.com>

--- vm/compiler/codegen/arm/CodegenDriver.cpp
+++ vm/compiler/codegen/arm/CodegenDriver.cpp
@@ -3704,7 +3704,7 @@ static void handlePCReconstruction(CompilationUnit *cUnit,
      * We should never reach here through fall-through code, so insert
      * a bomb to signal troubles immediately.
      */
-    if (numElems) {
+    if ((numElems) || (cUnit->jitMode == kJitLoop)) {
         newLIR0(cUnit, kThumbUndefined);
     }
 
@@ -4360,6 +4360,7 @@ void dvmCompilerMIR2LIR(CompilationUnit *cUnit)
                                  (LIR *) cUnit->loopAnalysis->branchToBody);
             dvmCompilerAppendLIR(cUnit,
                                  (LIR *) cUnit->loopAnalysis->branchToPCR);
+            cUnit->loopAnalysis->branchesAdded = true;
         }
 
         if (headLIR) {
